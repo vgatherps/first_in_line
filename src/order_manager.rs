8d48c1e1614b4a26355f937b16b9f8d151d4963c
vgatherps <vgatherps@gmail.com>
2020-05-08 14:24:12 -0700

    Remove unneeded bprint

--- src/order_manager.rs
+++ src/order_manager.rs
@@ -206,7 +206,6 @@ impl OrderManager {
 
     pub fn give_id<P: SidedPrice + Debug>(&mut self, price: &P, id: usize, amount: f64) {
         assert_ne!(id, 0);
-        println!("{:?}", self);
         match P::SIDE {
             Side::Buy => {
                 let price = price.to_buy();
