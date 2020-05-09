826b1cce78c2403b0329499603a5b4e5779abec9
EC2 Default User <ec2-user@ip-172-31-3-81.eu-central-1.compute.internal>
2020-05-09 15:21:56 +0000

    Fix dollars logic in filtering

--- src/tactic.rs
+++ src/tactic.rs
@@ -164,16 +164,19 @@ impl<'a> Tactic<'a> {
         // advance the book iterator until we find a level where we aren't most of it
         // Since we wait ~3 seconds to cancel orders in this fashion, it should be relatively
         // in-sync
-        let bid = bids.filter(|(prc, sz)| {
+        let bid = bids.filter(|(prc, dollars)| {
             let our_size = self.order_manager.buy_size_at(**prc);
+            let our_dollars = our_size * prc.unsigned() as f64;
+            // order manager thinks in dollars, we think in coins
             // inpractice, we are so small that this will be a test of alone or not
-            our_size / **sz < 0.7
-        }).next().map(|(prc, _)| *prc).unwrap_or(BuyPrice::new(1));
-        let ask = asks.filter(|(prc, sz)| {
+            our_dollars / **dollars < 0.7
+        }).next().map(|(prc, _)| *prc).unwrap_or(BuyPrice::new(0));
+        let ask = asks.filter(|(prc, dollars)| {
             let our_size = self.order_manager.sell_size_at(**prc);
+            let our_dollars = our_size * prc.unsigned() as f64;
             // inpractice, we are so small that this will be a test of alone or not
-            our_size / **sz < 0.7
-        }).next().map(|(prc, _)| *prc).unwrap_or(SellPrice::new(1000*1000*1000));
+            our_dollars / **dollars < 0.7
+        }).next().map(|(prc, _)| *prc).unwrap_or(SellPrice::new(1000*1000*1000*1000usize));
 
         (bid, ask)
     }
