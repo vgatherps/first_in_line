afd776fa88af6559f42752b2c3c2e0ea0b0d8300
vgatherps <vgatherps@gmail.com>
2020-05-27 18:06:14 -0700

    Log burst rate

--- src/logger.rs
+++ src/logger.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::Write;
 use std::io::BufWriter;
-use std::path::Path;
 use exchange::{
     bitmex_connection
 };
@@ -17,14 +16,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let mut bitmex_data = bitmex_connection().await;
     let mut out = BufWriter::new(File::create("bitmex_data.out").expect("Couldn't open file"));
+    let mut bitmex_data = bitmex_connection().await;
+    bitmex_data.next().await;
 
+    let mut rounds = 0;
+    let mut start = std::time::SystemTime::now();
     loop {
+        rounds += 1;
         let msg = bitmex_data.next().await;
         let encoded: Vec<u8> = bincode::serialize(&msg).unwrap();
+        if rounds >= 500 {
+            let end = std::time::SystemTime::now();
+            if let Ok(diff) = end.duration_since(start) {
+                let rounds = rounds as f64;
+                let time = diff.as_millis() as f64 / 1000.0;
+                println!("Last burst got {} messages / second", rounds / time);
+            }
+            start = std::time::SystemTime::now();
+            rounds = 0;
+        }
         out.write_all(&encoded).unwrap();
     }
-    
-    Ok(())
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,21 +1,17 @@
 use std::fs::File;
-use std::io::{Read, BufRead};
+use std::io::BufRead;
 use std::io::BufReader;
-use std::path::Path;
 use exchange::{
-    bitmex_connection,
     normalized
 };
 
 mod exchange;
 
 fn main() {
-    println!("Got here0");
     let mut out = BufReader::new(File::open("bitmex_data.out").expect("Couldn't open file"));
 
-    println!("Got here");
     while out.fill_buf().expect("Bad file").len() > 0 {
         let data: normalized::MarketEventBlock = bincode::deserialize_from(&mut out).expect("Got actual market data");
-        println!("Got {:?}", data);
+        println!("{:?}", data);
     }
 }
